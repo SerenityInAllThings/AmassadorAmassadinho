d5d47f6d4d16ae92a3bdd1e0a1a93413d16c2132
Peterson V <petersonvgama@gmail.com>
2024-07-28 14:08:37 -0300

    Adicionando minificação de HTML

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { ViteMinifyPlugin } from "vite-plugin-minify"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteMinifyPlugin()],
   publicDir: "./public",
 })
